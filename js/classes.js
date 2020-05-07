23e7f4480f0798a469fc0732ac73436510574a93
dashedman <assarbsmail@gmail.com>
2020-05-07 10:31:13 +0300

    expand struct

---
+++ js/classes.js
@@ -0,0 +1,267 @@
+//CLASSES
+class Texture{
+  constructor(path){
+		this.data = new Image()
+		this.data.src = path
+  }
+}
+class AnimationTexture extends Texture{
+  constructor(path,n=1,fspeed=10){
+		super(path)
+
+    this.frames = n
+    this.frameSpeed = fspeed
+		this.frameWidth = 1
+
+    this.data.onload = ()=>{
+			console.log("huh",path,this)
+    	this.frameWidth = Math.floor(this.data.width / n)
+    }
+  }
+}
+
+
+
+class Entity{
+  constructor(x=0,y=0,w=1,h=1,type=0){
+    this.x = x
+    this.y = y
+    this.width = w
+    this.height = h
+
+		this.type = type
+  }
+	collide(obj){
+		switch(this.type){
+			case TYPE_BOX:
+				switch (obj.type) {
+					case TYPE_BOX:
+						//collision flag
+						if(obj.x+obj.width < this.x ||
+							obj.x > this.x+this.width ||
+							obj.y+obj.height < this.y ||
+							obj.y > this.y+this.height)return;
+
+						//collision reaction
+						if(obj.speedX>0){
+							obj.x = this.x-obj.width
+						}else if(obj.speedX<0){
+							obj.x = this.x+this.width
+						}
+
+						if(obj.speedY<0){
+							obj.y = this.y+this.height
+							obj.speedY = 0
+						}else if(obj.speedY>0){
+							obj.speedY = 0
+							obj.y = this.y - obj.height
+							obj.onFloor = 1
+						}
+						break
+				}
+				break
+		}
+	}
+}
+
+
+class Sprite extends Entity{
+  constructor(x=0,y=0,w=0,h=0,source_path="",type=0){
+    super(x,y,w,h,type)
+    this.texture = TEXTURE_LIST[source_path]
+  }
+  draw(){
+    ctx.drawImage(this.texture.data,
+									0,
+									0,
+									this.texture.data.width,
+									this.texture.data.height,
+									this.x,
+									this.y,
+									this.width,
+									this.height)
+  }
+  update(dt){}
+}
+class ASprite extends Sprite{
+  constructor(x=0,y=0,w=0,h=0,source_path="",type=0){
+    super(x,y,w,h,source_path="",type)
+    this.frameStatus = 0
+  }
+  draw(){
+    ctx.drawImage(this.texture.data,
+                  Math.floor(this.frameStatus/this.texture.frameSpeed)*this.texture.frameWidth,
+                  0,
+                  this.texture.frameWidth,
+                  this.texture.data.height,
+									this.x,
+									this.y,
+									this.width,
+									this.height)
+  }
+  update(dt){
+    this.frameStatus = (this.frameStatus+dt*100)%(this.texture.frames*this.texture.frameSpeed)
+  }
+}
+
+
+class Alive extends Entity{
+  constructor(x=0,y=0,w=0,h=0,source_path=""){
+    super(x,y,w,h)
+    this.speedX = 0
+    this.speedY = 0
+		this.accelerationX = 10
+    this.direction = 1
+		this.onFloor = 0
+
+    this.entityState = 0
+    this.frameStatus = 0
+
+    this.animation_stay = TEXTURE_LIST[source_path+"_stay"]
+    this.animation_run = TEXTURE_LIST[source_path+"_run"]
+    this.animation_fall = TEXTURE_LIST[source_path+"_fall"]
+    this.animation_jump = TEXTURE_LIST[source_path+"_jump"]
+  }
+
+  draw(){
+      switch(this.entityState){
+        case ANIMATION_STATE_STAY:
+          ctx.drawImage(this.animation_stay.data,
+                        this.animation_stay.frameWidth*(this.direction*0.5-0.5) + Math.floor(this.frameStatus/this.animation_stay.frameSpeed)*this.animation_stay.frameWidth,
+                        0,
+                        (this.direction) * this.animation_stay.frameWidth,
+                        this.animation_stay.data.height,
+												this.x,
+												this.y,
+												this.width,
+												this.height)
+          break
+        case ANIMATION_STATE_FALL:
+          ctx.drawImage(this.animation_fall.data,
+                        this.animation_fall.frameWidth*(this.direction*0.5-0.5) + Math.floor(this.frameStatus/this.animation_fall.frameSpeed)*this.animation_fall.frameWidth,
+                        0,
+                        (this.direction) * this.animation_fall.frameWidth,
+                        this.animation_fall.data.height,
+												this.x,
+	                      this.y,
+	                      this.width,
+	                      this.height)
+          break
+        case ANIMATION_STATE_RUN:
+          ctx.drawImage(this.animation_run.data,
+                        this.animation_run.frameWidth*(this.direction*0.5-0.5) + Math.floor(this.frameStatus/this.animation_run.frameSpeed)*this.animation_run.frameWidth,
+                        0,
+                        (this.direction) * this.animation_run.frameWidth,
+                        this.animation_run.data.height,
+                        this.x,
+                        this.y,
+                        this.width,
+                        this.height)
+          break
+        case ANIMATION_STATE_JUMP:
+          ctx.drawImage(this.animation_jump.data,
+                        this.animation_jump.frameWidth*(this.direction*0.5-0.5) + Math.floor(this.frameStatus/this.animation_jump.frameSpeed)*this.animation_jump.frameWidth,
+                        0,
+                        (this.direction) * this.animation_jump.frameWidth,
+                        this.animation_jump.data.height,
+												this.x,
+												this.y,
+												this.width,
+												this.height)
+          break
+      }
+    }
+
+    update = function(dt){
+			switch(this.entityState){
+					case ANIMATION_STATE_STAY:
+	      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_stay.frames*this.animation_stay.frameSpeed)
+						break
+					case ANIMATION_STATE_FALL:
+	      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_fall.frames*this.animation_fall.frameSpeed)
+						break
+					case ANIMATION_STATE_RUN:
+	      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_run.frames*this.animation_run.frameSpeed)
+						break
+					case ANIMATION_STATE_JUMP:
+	      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_jump.frames*this.animation_jump.frameSpeed)
+						break
+	    }
+    }
+}
+
+
+class Hero extends Alive{
+	constructor(x=0,y=0,w=0,h=0,source_path=""){
+    super(x,y,w,h,source_path)
+  }
+  update = function(dt){
+    var newFlag = false
+		var runFlag = false
+		var fallFlag = false
+
+    //ХОДИТ
+		if( PRESSED_KEYS[ KEY_LEFT ] || PRESSED_KEYS[ KEY_A ] ) {
+			runFlag = true
+			this.speedX = -100
+			this.x += this.speedX * dt
+      this.direction = -1
+		}
+		if( PRESSED_KEYS[ KEY_RIGHT ] || PRESSED_KEYS[ KEY_D ] ) {
+			runFlag = true
+			this.speedX = 100
+      this.x += this.speedX * dt
+      this.direction = 1
+		}
+
+      //Физика прыжжка
+		if( PRESSED_KEYS[KEY_SPACE] && this.onFloor){
+			this.speedY = 100
+			this.onFloor -= 1
+		}
+		//this.speedY = this.speedY - dt*9.8
+		this.y -= this.speedY * dt
+
+		for(let i=0;i<LIFELESSES.length;i++){
+			LIFELESSES.collide(this)
+		}
+
+		if(this.speedY>0){
+			if(this.entityState != ANIMATION_STATE_FALL){
+				newFlag = true
+				this.entityState = ANIMATION_STATE_FALL
+			}
+		}
+		else if(this.speedY<0){
+			if(this.entityState != ANIMATION_STATE_JUMP){
+				newFlag = true
+				this.entityState = ANIMATION_STATE_JUMP
+			}
+		}else	if(runFlag){
+			if(this.entityState != ANIMATION_STATE_RUN){
+				newFlag = true
+        this.entityState = ANIMATION_STATE_RUN
+			}
+		}else{
+			if(this.entityState != ANIMATION_STATE_STAY){
+				newFlag = true
+        this.entityState = ANIMATION_STATE_STAY
+			}
+		}
+    if(newFlag) this.frameStatus=0;
+    else switch(this.entityState){
+				case ANIMATION_STATE_STAY:
+      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_stay.frames*this.animation_stay.frameSpeed)
+					break
+				case ANIMATION_STATE_FALL:
+      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_fall.frames*this.animation_fall.frameSpeed)
+					break
+				case ANIMATION_STATE_RUN:
+      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_run.frames*this.animation_run.frameSpeed)
+					break
+				case ANIMATION_STATE_JUMP:
+      		this.frameStatus = (this.frameStatus+dt*100)%(this.animation_jump.frames*this.animation_jump.frameSpeed)
+					break
+    }
+	}
+}
---
+++ js/constants.js
@@ -0,0 +1,62 @@
+//CONSTANTS
+//other
+const DATA_PATH = "resources/"
+const DEBUG = true
+
+//arrays
+const PRESSED_KEYS = new Array(128)
+const LIFELESSES = {}
+const BACKGROUNDS = {}
+const ALIVES = {}
+const TEXTURE_LIST = {}
+
+//animation states
+const ANIMATION_STATE_STAY_L=0
+const ANIMATION_STATE_STAY_R=1
+const ANIMATION_STATE_FALL_L=2
+const ANIMATION_STATE_FALL_R=3
+const ANIMATION_STATE_RUN_L=4
+const ANIMATION_STATE_RUN_R=5
+const ANIMATION_STATE_JUMP_L=6
+const ANIMATION_STATE_JUMP_R=7
+
+//block types
+const TYPE_BOX = 0
+const TYPE_CIRCLE = 1
+const TYPE_TRIANGLE_LEFT_UP = 2
+const TYPE_TRIANGLE_RIGHT_UP = 3
+const TYPE_TRIANGLE_RIGHT_DOWN = 4
+const TYPE_TRIANGLE_LEFT_DOWN = 5
+
+//keycodes
+const KEY_SPACE = 32
+const KEY_LEFT = 37
+const KEY_UP = 38
+const KEY_RIGHT = 39
+const KEY_DOWN = 40
+const KEY_A = 65
+const KEY_B = 66
+const KEY_C = 67
+const KEY_D = 68
+const KEY_E = 69
+const KEY_F = 70
+const KEY_G = 71
+const KEY_H = 72
+const KEY_I = 73
+const KEY_J = 74
+const KEY_K = 75
+const KEY_L = 76
+const KEY_M = 77
+const KEY_N = 78
+const KEY_O = 79
+const KEY_P = 80
+const KEY_Q = 81
+const KEY_R = 82
+const KEY_S = 83
+const KEY_T = 84
+const KEY_U = 85
+const KEY_V = 86
+const KEY_W = 87
+const KEY_X = 88
+const KEY_Y = 89
+const KEY_Z = 90
